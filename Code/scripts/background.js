2b959b746d0988407884af5c2f6bb1d76ab4c044
Rui Jiang <rjiang.bb@gmail.com>
2011-04-17 15:13:33 -0500

    - Added Chat and Notifications mockup to options. - Added tooltips for "Account name" and "API token".

--- Code/scripts/background.js
+++ Code/scripts/background.js
@@ -1,5 +1,3 @@
-var happycamper = {};
-
 happycamper.settings = {};
 
 happycamper.state = {
--- Code/scripts/options.js
+++ Code/scripts/options.js
@@ -1,16 +1,14 @@
-var happycamper = {};
-
 // just defaults
 happycamper.settings = {
     me: null,
     refreshInterval: 10,
-    notifications: {
-        enabled: true,
-        showFor: 10
-    },
     chat: {
         showTimestamps: true,
         showEnterLeave: true
+    },
+    notifications: {
+        enabled: true,
+        showFor: 10
     }
 };
 
@@ -19,6 +17,7 @@ happycamper.options = function() {
     this.initialize = function() {
         initializeSettings();
         wireTabs();
+        qtipWhatsThis();
     };
 
     // private
@@ -30,14 +29,49 @@ happycamper.options = function() {
             happycamper.util.saveJson("settings", happycamper.settings);
         } else {
             happycamper.settings = settings;
-        }    
+        }
     }
 
     function wireTabs() {
-        
+        $("ul.menu li a").click(function() {
+            $("ul.menu li").removeClass("selected");
+            $(this).parent("li").addClass("selected");
+
+            $("div.content").hide();
+            $("div.content" + $(this).attr("href")).show();
+
+            return false;
+        });
+    }
+
+    function qtipWhatsThis() {
+        createTooltip($("div.what a.account"), $("#what-account-name-template").tmpl());
+        createTooltip($("div.what a.token"), $("#what-api-token-template").tmpl())
+    }
+
+    function createTooltip(element, content) {
+        // disable click navigation as well
+        element.click(function() { return false; }).qtip({
+            content: {
+                text: content
+            },
+            position: {
+                my: "bottom right",
+                at: "top right"
+            },
+            style: {
+                classes: "ui-tooltip-light ui-tooltip-shadow",
+                tip: {
+                    corner: true,
+                    width: 10,
+                    height: 10
+                }
+            }
+        });
     }
 };
 
 $(function() {
+    happycamper.options = new happycamper.options();
     happycamper.options.initialize();
 });
