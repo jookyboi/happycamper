62d08b447d86c96dbb45fb5af98fd7300568974a
Rui Jiang <rjiang.bb@gmail.com>
2011-04-18 21:47:35 -0500

    - Added code to load settings.

--- Code/scripts/options.js
+++ Code/scripts/options.js
@@ -1,5 +1,9 @@
 // just defaults
 happycamper.settings = {
+    account: {
+        name: "ruijiang",
+        apiToken: "1eb3d67b287357b919ccb88f83056a636a7a9e5e"
+    },
     me: null,
     refreshInterval: 10,
     chat: {
@@ -8,19 +12,34 @@ happycamper.settings = {
     },
     notifications: {
         enabled: true,
-        showFor: 10
+        showFor: 30
     }
 };
 
 happycamper.options = function() {
+    // ui elements
+    var $accountName, $apiToken;
+    var $showTimestamps, $showEnterLeave;
+    var $notificationsEnabled, $notificationsShowFor;
+
     // public
     this.initialize = function() {
+        initializeUI();
         initializeSettings();
         wireTabs();
         qtipWhatsThis();
     };
 
-    // private
+    // initialize
+    function initializeUI() {
+        $accountName = $("input[name='account-name']");
+        $apiToken = $("input[name='api-token']");
+        $showTimestamps = $("input[name='show-timestamps']");
+        $showEnterLeave = $("input[name='show-enter-leave']");
+        $notificationsEnabled = $("input[name='notifications-enabled']");
+        $notificationsShowFor = $("select[name='notifications-show-for']");
+    }
+
     function initializeSettings() {
         var settings = happycamper.util.loadJson("settings");
 
@@ -30,8 +49,39 @@ happycamper.options = function() {
         } else {
             happycamper.settings = settings;
         }
+
+        loadSettingsForUI();
     }
 
+    function loadSettingsForUI() {
+        var settings = happycamper.settings;
+
+        $accountName.val(settings.account.name);
+        $apiToken.val(settings.account.apiToken);
+
+        if (settings.chat.showTimestamps) {
+            $showTimestamps.attr("checked", "checked");
+        } else {
+            $showTimestamps.removeAttr("checked");
+        }
+
+        if (settings.chat.showEnterLeave) {
+            $showEnterLeave.attr("checked", "checked");
+        } else {
+            $showEnterLeave.removeAttr("checked");
+        }
+
+        if (settings.notifications.enabled) {
+            $notificationsEnabled.attr("checked", "checked");
+        } else {
+            $notificationsEnabled.removeAttr("checked");
+        }
+
+        $notificationsShowFor.val(settings.notifications.showFor);
+    }
+
+
+    // tabs
     function wireTabs() {
         $("ul.menu li a").click(function() {
             $("ul.menu li").removeClass("selected");
@@ -44,6 +94,7 @@ happycamper.options = function() {
         });
     }
 
+    // utilities
     function qtipWhatsThis() {
         createTooltip($("div.what a.account"), $("#what-account-name-template").tmpl());
         createTooltip($("div.what a.token"), $("#what-api-token-template").tmpl())
